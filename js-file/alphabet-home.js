8f57d7a5004ea0e691caf4b29b0908c670ec550d
Saeedtamim <saeedtamim07@gmail.com>
2024-02-15 11:25:46 +0000

    score update and life score update

--- js-file/alphabet-home.js
+++ js-file/alphabet-home.js
@@ -1,3 +1,34 @@
+// Keyboard press
+function handlerKeyboardButtonPress(event) {
+    const userPress = event.key;
+
+    // find key press
+    const currentAlphabetPress = document.getElementById('random-alphabet-showing');
+    const currentAlphabetText = currentAlphabetPress.innerText;
+    const asExpectedAlphabet = currentAlphabetText.toLocaleLowerCase();
+    console.log('User pressed :', userPress, '& Random number is :', asExpectedAlphabet);
+
+    // match the user key
+    if (userPress === asExpectedAlphabet) {
+        console.log('You got a point');
+
+        const currentScores = getTextElementById('score-board');
+        const updatedscores = currentScores +1;
+        setTextValueById('score-board', updatedscores);
+
+        console.log('Rightly press :', asExpectedAlphabet);
+        removeBackgroundById(asExpectedAlphabet);
+        countinueGame();
+    }
+    else {
+        const currentLife = getTextElementById('lifeline');
+        const updateLifline = currentLife - 1;
+        setTextValueById('lifeline',updateLifline);
+        console.log('You lost.')
+    }
+}
+document.addEventListener('keyup', handlerKeyboardButtonPress);
+
 // Gaming loop
 function countinueGame(){
     const alphabet = randomAlphabe();
--- js-file/utilities.js
+++ js-file/utilities.js
@@ -1,62 +1,49 @@
-// Keyboard press
-function handlerKeyboardButtonPress(event){
-    const userPress = event.key;
-
-    // find key press
-    const currentAlphabetPress = document.getElementById('random-alphabet-showing');
-    const currentAlphabetText = currentAlphabetPress.innerText;
-    const asExpectedAlphabet = currentAlphabetText.toLocaleLowerCase();
-    console.log('User pressed :', userPress, '& Random number is :', asExpectedAlphabet);
-
-    // match the user key
-    if(userPress === asExpectedAlphabet){
-        console.log('You got a point');
-        console.log('Rightly press :', asExpectedAlphabet);
-        removeBackgroundById(asExpectedAlphabet);
-        countinueGame();
-    }
-    else{
-        console.log('You lost.')
-    }
+// set score
+function getTextElementById(elementId) {
+    const element = document.getElementById(elementId);
+    const elementText = element.innerText;
+    const updateElement = parseInt(elementText);
+    return updateElement;
 }
-document.addEventListener('keyup', handlerKeyboardButtonPress);
-
-
+function setTextValueById(elementId, value){
+    const element = document.getElementById(elementId);
+    element.innerText = value;
+}
 
 
 // hide elements
-function hideElementById (elementId){
+function hideElementById(elementId) {
     const element = document.getElementById(elementId);
-    element.classList.add('hidden'); 
+    element.classList.add('hidden');
 }
 
 // // show element
-function showElementById (elementId){
+function showElementById(elementId) {
     const element = document.getElementById(elementId);
     element.classList.remove('hidden')
 }
 
 // find a random alphabets function
-function randomAlphabe(){
-    const allAlphabet ='abcdefghijklmnopqrstuvwxyz'; 
+function randomAlphabe() {
+    const allAlphabet = 'abcdefghijklmnopqrstuvwxyz';
     const alphabets = allAlphabet.split('');
 
     // math
-    const randomMath = Math.random()*25;
+    const randomMath = Math.random() * 25;
     const index = Math.round(randomMath);
 
-    const findAlphabet =alphabets[index];
+    const findAlphabet = alphabets[index];
     // console.log(findAlphabet);
-    return findAlphabet ;
+    return findAlphabet;
 }
 
 // set bg
-function setBackgroundById(elementId){
+function setBackgroundById(elementId) {
     const element = document.getElementById(elementId);
     element.classList.add('bg-orange-400');
 }
 // remove bg
-function removeBackgroundById(elementId){
+function removeBackgroundById(elementId) {
     const element = document.getElementById(elementId);
     element.classList.remove('bg-orange-400');
 }
